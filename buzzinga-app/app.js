ba0842c4fa4b7e8e2a2d73a334ef338b94dab49e
Clemens Hahn (couchbits GmbH) <clemens@couchbits.com>
2020-02-22 14:05:54 +0100

    exchange http-server and web-socket libraries. kudos: https://blog.kevinchisholm.com/javascript/node-js/websocket-server-five-minutes/

--- buzzinga-app/app.js
+++ buzzinga-app/app.js
@@ -1,119 +1,59 @@
-// http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 "use strict";
 
 // Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'node-chat';
 
-// Port where we'll run the websocket server
-var webSocketsServerPort = 1337;
+const serverPort = 3000,
+    http = require("http"),
+    express = require("express"),
+    app = express(),
+    server = http.createServer(app),
+    WebSocket = require("ws"),
+    websocketServer = new WebSocket.Server({ server }),
+    buzzBuzzers = require('../buzz-buzzers/src/index'),
+    buzzers = buzzBuzzers(); // initialize buzzers
 
-// websocket and http servers
-var webSocketServer = require('websocket').server;
-var http = require('http');
+//when a websocket connection is established
+websocketServer.on('connection', (webSocketClient) => {
+    //send feedback to the incoming connection
+    webSocketClient.send(JSON.stringify({ type:'status', data: { "connection" : "ok"} }));
 
-/**
- * Global variables
- */
-// list of currently connected clients (users)
-var clients = [ ];
+    //when a message is received
+    webSocketClient.on('message', (message) => {
 
-// for serving files via HTTP
-var fs = require('fs')
+        //for each websocket client
+        websocketServer
+          .clients
+          .forEach( client => {
+              //send the client the current message
+              client.send(`{ "message" : ${message} }`);
+          });
+    });
 
-/**
- * HTTP server
- */
-var server = http.createServer(function(req, res) {
-  // serve files in this dir. kudos: https://nodejs.org/en/knowledge/HTTP/servers/how-to-serve-static-files/
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-    console.log("Served " + __dirname + req.url)
-  });
+    // Get notified when a button is pressed
+    buzzers.onPress(function(ev) {
+      // ev is an object with two attributes:
+      // - controller: Number from 1 to 4
+      // - button: Number from 0 to 4. 0 is the big red button.
+      var msg = 'Button ' + ev.button + ' on controller ' + ev.controller + ' pressed'
+      console.log(msg);
+      var obj = {
+              time: (new Date()).getTime(),
+              text: msg,
+              button: ev.button,
+              controller: ev.controller
+          };
+      var json = JSON.stringify({ type:'buzz-event', data: obj });
+      webSocketClient.send(json);
+    });
 });
-server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+
+// serve static files in directory `static`
+app.use(express.static('static'))
+//start the web server
+server.listen(serverPort, () => {
+    console.log(`Server started on port ` + serverPort);
     identifyBuzzers();
-});
-
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket request is just
-    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
-});
-
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-
-    // accept connection - you should check 'request.origin' to make sure that
-    // client is connecting from your website
-    // (http://en.wikipedia.org/wiki/Same_origin_policy)
-    var connection = request.accept(null, request.origin);
-    // we need to know client index to remove them on 'close' event
-    var index = clients.push(connection) - 1;
-
-    console.log((new Date()) + ' Connection accepted.');
-
-    // user sent some message
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') { // accept only text
-            // log and broadcast the message
-            console.log((new Date()) + ' Received Message from: ' + message.utf8Data);
-            // broadcast message to all connected clients
-            var obj = {
-                    time: (new Date()).getTime(),
-                    text: message.utf8Data
-                };
-            var json = JSON.stringify({ type:'message', data: obj });
-            for (var i=0; i < clients.length; i++) {
-                clients[i].sendUTF(json);
-            }
-        }
-    });
-
-    // user disconnected
-    connection.on('close', function(connection) {
-        console.log((new Date()) + " Peer "
-                + connection.remoteAddress + " disconnected.");
-        // remove user from the list of connected clients
-        clients.splice(index, 1);
-    });
-
-});
-
-// #############################################################################
-// buzz-buzzers
-// #############################################################################
-var buzzBuzzers = require('../buzz-buzzers/src/index')
-var buzzers = buzzBuzzers(); // initialize buzzers
-
-// Get notified when a button is pressed
-buzzers.onPress(function(ev) {
-	// ev is an object with two attributes:
-	// - controller: Number from 1 to 4
-	// - button: Number from 0 to 4. 0 is the big red button.
-  var msg = 'Button ' + ev.button + ' on controller ' + ev.controller + ' pressed'
-	console.log(msg);
-  var obj = {
-          time: (new Date()).getTime(),
-          text: msg,
-          button: ev.button,
-          controller: ev.controller
-      };
-  var json = JSON.stringify({ type:'message', data: obj });
-  for (var i=0; i < clients.length; i++) {
-      clients[i].sendUTF(json);
-  }
 });
 
 // Get notified when a button is released
--- buzzinga-app/static/script/split-view.js
+++ buzzinga-app/static/script/split-view.js
@@ -6,6 +6,12 @@ $(function () {
     var buzzOneAudio = document.getElementById("audio-left");
     var buzzTwoAudio = document.getElementById("audio-right");
 
+    function mirror() {
+      let mirrored = new URLSearchParams(window.location.search).has('mirrored');
+      console.log("Mirrored? " + mirrored)
+      if (! mirrored) return;
+    }
+
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
@@ -16,7 +22,7 @@ $(function () {
     }
 
     // open connection
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var connection = new WebSocket('ws://127.0.0.1:3000');
 
     connection.onopen = function () {
         console.log("connection opened: " + connection.readyState);
@@ -24,30 +30,30 @@ $(function () {
 
     connection.onerror = function (error) {
         // just in there were some problems with conenction...
-        console.warn("Sorry, but there\'s some problem with your connection or the server is down.");
+        console.warn("Sorry, but there\'s some problem with your connection or the server is down. " + error);
     };
 
     // most important part - incoming messages
-    connection.onmessage = function (message) {
-        try {
-            var json = JSON.parse(message.data);
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message.data);
-            return;
-        }
-        console.log("Received " + JSON.stringify(json));
+    connection.onmessage = function (json) {
+      let message = JSON.parse(json.data);
+      if (message.type == 'status') {
+        console.log("Status message received: %o", message.data);
+        return;
+      }
+      if (message.type == 'buzz-event') {
+        console.log("Received buzz-event  %o", message.data);
 
-        if (json.data.button != 0) {
-            console.log("Player " + json.data.controller + " pressed the wrong button (was " + json.data.button + ")")
+        if (message.data.button != 0) {
+            console.log("Player " + message.data.controller + " pressed the wrong button (was " + message.data.button + ")")
             return;
         }
         if (lock) {
-          console.log(json.data.controller + " was too late!");
+          console.log(message.data.controller + " was too late!");
           return;
         }
         lock = true;
-        if (json.data.button !== null) {
-          switch (json.data.controller) {
+        if (message.data.button !== null) {
+          switch (message.data.controller) {
             case 1:
               $('.one').addClass('one-wins');
               buzzOneAudio.play();
@@ -57,8 +63,9 @@ $(function () {
               buzzTwoAudio.play();
               break;
             default:
-              console.warn("Controller " + json.data.contoller + " not supported!")
+              console.warn("Controller " + message.data.contoller + " not supported!")
           }
         }
+      }
     };
 });
