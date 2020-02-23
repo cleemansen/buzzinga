49d995039ad636f4e908d016664d63ebf409afc0
Clemens Hahn (couchbits GmbH) <clemens@couchbits.com>
2020-02-23 11:54:19 +0100

    housekeeping: structure of file

--- buzzinga-app/app.js
+++ buzzinga-app/app.js
@@ -15,6 +15,13 @@ const serverPort = 3000,
     ;
 let roundLocked = false // first pressed controller wins!
 
+// serve static files in directory `static`
+app.use(express.static('static'))
+//start the web server
+server.listen(serverPort, () => {
+    console.log(`Server started on port ` + serverPort);
+    identifyBuzzers();
+});
 
 //when a websocket connection is established
 websocketServer.on('connection', (webSocketClient) => {
@@ -75,33 +82,9 @@ websocketServer.on('connection', (webSocketClient) => {
     });
 });
 
-// serve static files in directory `static`
-app.use(express.static('static'))
-//start the web server
-server.listen(serverPort, () => {
-    console.log(`Server started on port ` + serverPort);
-    identifyBuzzers();
-});
-
 // Get notified when a button is released
 buzzers.onRelease(function(ev) {
-	console.log('Button ' + ev.button + ' on controller ' + ev.controller + ' released');
-});
-
-// Get notified whenever something changes
-buzzers.onChange(function(state) {
-	// state is an array of booleans with all buttons
-	// false means the button is not pressed
-	// and true when a button is pressed
-	/* An example could look like this, in this case the second color button
-	of controller 2 was pressed and the big red button on controller four is pressed
-	[
-        false, false, false, false, false, // first controller
-        false, false, true, false, false, // second controller
-        false, false, false, false, false, // third controller
-        true, false, false, false, false // fourth controller
-    ]
-	*/
+  // console.log('Button ' + ev.button + ' on controller ' + ev.controller + ' released');
 });
 
 // Get notified when an error happens
